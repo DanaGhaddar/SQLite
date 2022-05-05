bd1ab0af8fce83aceda479f17e1f7a88b6be9a42
Dana Ghaddar <dana.ghaddar@lau.edu>
2022-05-05 22:59:24 +0300

    initializing list view in java

--- app/src/main/java/com/lau2022/sqlite/MainActivity.java
+++ app/src/main/java/com/lau2022/sqlite/MainActivity.java
@@ -3,13 +3,26 @@ package com.lau2022.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView Lview;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lview = (ListView) findViewById(R.id.list);
+        arrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
+        Lview.setAdapter(adapter);
+
+
+
     }
 }
